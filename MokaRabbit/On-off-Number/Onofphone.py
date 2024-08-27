43a08cdc72c72ce468e5f15899a5d100601ad99b
G0LD R4BB1T <goldrabbit907@gmail.com>
2024-08-27 21:02:55 +0200

    On off

---
+++ MokaRabbit/On-off-Number/Onofphone.py
@@ -0,0 +1,29 @@
+import socket
+import os
+import sys
+import threads
+import time
+import requests
+
+def detect_onOff_numberphone():
+    print(detect_onOff_numberphone)
+
+def identified_OnOff_numberphone():
+   print(identified_OnOff_numberphone)
+def access_device_of_onOff_numberphone():
+    print(access_device_of_onOff_numberphone)
+def  access_account_of_OnOff_numberphone():
+      print(access_account_of_OnOff_numberphone)
+
+def control_devices_of_onOff_numberphone():
+    print(control_devices_of_onOff_numberphone)
+def access_screen_stream_live_of_OnOff_numberphone():
+    print(access_screen_stream_live_of_OnOff_numberphone)
+def encrypt_device_of_OnOff_numberphone():
+   print(encrypt_device_of_OnOff_numberphone)
+
+def encrypt_devices_of_OnOff_numberphone():
+    print(encrypt_devices_of_OnOff_numberphone)
+
+def display_first_name_lastname_and_birthday_of_masked_numberphone():
+    print(display_first_name_lastname_and_birthday_of_masked_numberphone)
