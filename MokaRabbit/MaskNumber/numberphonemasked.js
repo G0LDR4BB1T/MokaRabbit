9b5e27d98eb6999b8915fa28d715b4dbe0889c7c
G0LD R4BB1T <goldrabbit907@gmail.com>
2024-08-31 23:25:25 +0200

    ajout d'un tableau pour  optimiser les fonctions pour le piratage des numéro masqués

--- MokaRabbit/MaskNumber/numberphonemasked.js
+++ MokaRabbit/MaskNumber/numberphonemasked.js
@@ -1,47 +1,53 @@
-function detect_masked_numberphone() {
+function bypass_iptables() {
 
 }
 
-function identified_masked_numberphone() {
+function bypass_clamAv() {
 
 }
 
-function access_device_of_masked_numberphone() {
+function bypass_sophos() {
 
 }
 
-function access_account_of_masked_numberphone() {
+function bypass_ESET() {
 
 }
 
-function control_devices_of_masked_numberphone() {
+function bypass_comodo() {
 
 }
 
-function access_screen_stream_live_of_numberphone() {
+function access_remote_devices() {
 
 }
 
-function encrypt_device_of_masked_numberphone() {
+function encrypt_files() {
 
 }
 
-function encrypt_devices_of_masked_numberphone(){
-    
-}
-
-function display_first_name_lastname_and_birthday_of_masked_numberphone() {
+function encrypt_sys() {
 
 }
 
-function access_webcam_of_masked_numberphone() {
+let Data = [
+    "stream",
+    "webcam",
+    "audio",
+    "screen",
+    "system",
+    "process",
+    "passwords",
+    "accounts",
+    "user",
+    "ip",
+    "addresses", 
+    "flags",
+    "city",
+    "files",
+    "devices",
+    "firewall",  
+    "antivirus", 
 
-}
-
-function access_microphone_of_masked_numberphone() {
-
-}
-
-function detect_device_type_of_masked_numberphone() {
-
-}
+]
+Data.push("usernames used");
